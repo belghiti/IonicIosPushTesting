096627be73db9322722ce9e1f854e239072a0acf
belghiti idriss <belghiti@192.168.32.128>
2018-11-17 09:02:56 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
@@ -10,12 +10,47 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private push: Push, private alertCtrl: AlertController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      const options: PushOptions = {
+        android: {},
+        ios: {
+            alert: 'true',
+            badge: true,
+            sound: 'false'
+        }
+     };
+     
+     const pushObject: PushObject = this.push.init(options);
+     
+     
+     pushObject.on('notification').subscribe((notification: any) => {
+       
+     });
+     
+     pushObject.on('registration').subscribe((registration: any) => {
+      let alert = this.alertCtrl.create({
+        title: 'Registration',
+        subTitle: registration.registrationId,
+        buttons: ['Dismiss']
+      });
+      alert.present();
+     });
+     
+     pushObject.on('error').subscribe(error => {
+      let alert = this.alertCtrl.create({
+        title: 'Error',
+        subTitle: error.message,
+        buttons: ['Dismiss']
+      });
+      alert.present();
+     });
+
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { Push } from '@ionic-native/push';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -22,6 +22,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    Push,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
